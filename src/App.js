152653b06728c47e73fe857d1d5c91f80910ae74
TriciaF <TriciaF@thinkful.com>
2018-12-09 23:12:05 -0500

    created app introduction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,12 +6,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-logo">E</h1>
+          <h1 className="App-title">Elisante Tours</h1>
         </header>
+        <div className="App-background">
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </div>
       </div>
     );
   }
